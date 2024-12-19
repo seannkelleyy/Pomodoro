50f174fcbce1636af601942974a7297b743bd42b
Sean Kelley <94130791+seannkelleyy@users.noreply.github.com>
2024-12-19 09:34:53 -0500

    Bug fixes and improvements 1 (#4)
    * Updates default timer break time
    
    * Fix(Footer): Puts footer at bottom of screen
    
    * Refact(Timer Button): Refactors if statement in changeTimerTypeButtons. Also updates css to make footer be positioned better.
    
    * Feat(Sound): Adds sound and updates footer

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Timer } from './pages/Timer'
+import { Footer } from './components/footer/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Timer />
+		<section title='Pomodoro Timer' className='pomodoro-timer'>
+			<Timer />
+			<Footer />
+		</section>
 	</React.StrictMode>,
 )
 
